df520d81c8174f3c265792ca09e823e2111eb837
Bekzhan Eldiiar uulu <eldiiar_b@auca.kg>
2022-10-01 10:53:18 +0200

    lab02, o07 wrong answer

---
+++ lab02/p07/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+template <typename C>
+int sz(const C &c) { return static_cast<int>(c.size()); }
+
+using namespace std;
+
+int main()
+{
+    iostream::sync_with_stdio(false);
+    bool doCicle = true;
+    while (doCicle)
+    {
+        int a = 0;
+        int b = 0;
+        cin >> a;
+        cin >> b;
+        if (a == -1 && b == -1)
+        {
+            doCicle = false;
+        }
+        int result = abs(a - b);
+        if ()
+            if (result1 > result2)
+            {
+                cout << result1 << endl;
+            }
+            else
+            {
+                cout << result2 << endl;
+            }
+    }
+}
