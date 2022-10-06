2ac6e08fc584e57cd874a100818f633422be81c1
Bekzhan Eldiiar uulu <eldiiar_b@auca.kg>
2022-10-06 11:58:01 +0200

    lab01, p03 completed

--- lab01/p03/main.cpp
+++ lab01/p03/main.cpp
@@ -7,22 +7,26 @@ int main()
 {
     const string noDataMessage = "no data";
     const int width = 30;
-    for (string name; cin >> name;){
-    
-    int nGrades = 0;
-    double sum = 0;
-    int grade;
-    while(cin >> grade){
-        nGrades++;
-        sum += grade;
-    }
-    cout << setfill('.') << fixed << setprecision(2);
-    if (sum == 0){
-        cout << left << setw(width) << name <<  "|" << right << setw(width) << noDataMessage << "\n";
-    }else {
-        cout << left << setw(width) << name <<  "|" << right << setw(width) << sum/nGrades << "\n";
-    }
-    cin.clear();
-    }
+    for (string name; cin >> name;)
+    {
 
+        int nGrades = 0;
+        double sum = 0;
+        int grade;
+        while (cin >> grade)
+        {
+            nGrades++;
+            sum += grade;
+        }
+        cout << setfill('.') << fixed << setprecision(2);
+        if (sum == 0)
+        {
+            cout << left << setw(width) << name << "|" << right << setw(width) << noDataMessage << "\n";
+        }
+        else
+        {
+            cout << left << setw(width) << name << "|" << right << setw(width) << sum / nGrades << "\n";
+        }
+        cin.clear();
+    }
 }
