3fc2c1be598c3ee281f575f1ef759bb4c9d31457
Bekzhan Eldiiar uulu <eldiiar_b@auca.kg>
2022-10-26 15:22:08 +0200

    lab01, p07 is in the process

---
+++ lab01/p07/main.cpp
@@ -0,0 +1,97 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "../../doctest/doctest.h"
+#include "string"
+
+using namespace std;
+
+TEST_CASE("default constructor")
+{
+    string n;
+    REQUIRE(n.empty() == true);
+    REQUIRE(n.size() == 0);
+}
+TEST_CASE("constructor with size and intial value")
+{
+    string str(4, 'i');
+    REQUIRE(str.at(3) == 'i');
+    REQUIRE(str.size() == 4);
+}
+TEST_CASE("copy constructor")
+{
+    string str = "string";
+    string str2 = str;
+    REQUIRE(str == str2);
+}
+TEST_CASE("at(index), exception")
+{
+    string str = "string";
+    REQUIRE_THROWS_AS(str.at(9), std::logic_error);
+}
+TEST_CASE("push_back()")
+{
+    string str;
+    str.push_back('a');
+    REQUIRE(str.at(0) == 'a');
+}
+TEST_CASE("pop_back()")
+{
+    string str = "string";
+    str.pop_back();
+    REQUIRE(str.back() == 'n');
+}
+TEST_CASE("+, += operators")
+{
+    string str;
+    str += 's';
+    str += 't';
+    str += 'r';
+    REQUIRE(str == "str");
+    string str2 = "ing";
+    str = str + str2;
+    REQUIRE(str == "string");
+}
+TEST_CASE("==, !=, <")
+{
+    string str = "string";
+    string str2 = "string";
+    string str3 = "is not a string";
+    REQUIRE(str == str2);
+    REQUIRE(str3 != str2);
+    REQUIRE(str.size() < str3.size());
+}
+TEST_CASE("(auto &c : str)")
+{
+    string str = "string";
+    string str2;
+    for (auto &c : str)
+    {
+        str2 += c;
+    }
+    REQUIRE(str == str2);
+}
+TEST_CASE("front(), back()")
+{
+    string str = "string";
+    REQUIRE(str.front() == 's');
+    REQUIRE(str.back() == 'g');
+}
+TEST_CASE("substr(index, length), find(char ch)")
+{
+    string str = "string";
+    REQUIRE(str.substr(0, 2) == "st");
+    string str2 = "s";
+    size_t found = str.find(str2);
+    REQUIRE(found == 0);
+}
+TEST_CASE("insert(index, length, char")
+{
+    string str = "string";
+    str.insert(6, 3, 'g');
+    REQUIRE(str == "stringggg");
+}
+TEST_CASE("erase(index, length)")
+{
+    string str = "string";
+    str.erase(3, 3);
+    REQUIRE(str == "str");
+}
