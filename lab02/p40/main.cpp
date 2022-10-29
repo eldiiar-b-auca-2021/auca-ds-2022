5d95d4cb5b166f09fe9f5aef7e50248522c672af
Bekzhan Eldiiar uulu <eldiiar_b@auca.kg>
2022-10-29 12:49:24 +0200

    lab02, p40 runtime error

--- lab02/p40/main.cpp
+++ lab02/p40/main.cpp
@@ -4,77 +4,87 @@ template <typename C>
 int sz(const C &c) { return static_cast<int>(c.size()); }
 
 using namespace std;
-
+bool isVowel(char &ch)
+{
+    switch (ch)
+    {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+    case 'A':
+    case 'E':
+    case 'I':
+    case 'O':
+    case 'U':
+        return true;
+    }
+    return false;
+}
 int main()
 {
     iostream::sync_with_stdio(false);
     string text;
 
-    string currentS;
     getline(cin, text);
 
     vector<string> str;
+    string temp;
 
-    for (int i = 0; i < (int)text.size(); i++)
+    for (int i = 0; i < sz(text); i++)
     {
-        if (text.at(i) != ' ')
+        if (text[i] != ' ')
         {
-            currentS += text.at(i);
+            temp += text[i];
         }
         else
         {
-            str.push_back(currentS);
-            currentS = "";
+            str.push_back(temp);
+            temp = "";
         }
     }
-    str.push_back(currentS);
-    currentS = "";
-    bool isVowel = false;
-    for (int i = 0; i < (int)str.size(); i++)
+    str.push_back(temp);
+    for (int i = 0; i < sz(str); i++)
     {
-        string temp = str[i];
-        switch (temp.at(0))
+        if (isVowel(str[i].at(0)))
         {
-        case 'a':
-        case 'e':
-        case 'i':
-        case 'o':
-        case 'u':
-        case 'A':
-        case 'E':
-        case 'I':
-        case 'O':
-        case 'U':
-            isVowel = true;
-        }
-        if (isVowel)
-        {
-            if (temp.at(temp.size() - 1) != '.')
+            string t;
+            if (str[i].at(str[i].size() - 1) != '.')
             {
-                currentS += temp;
-                currentS += "ay";
-                currentS += " ";
+                t = str[i] + "ay";
+                str[i] = t;
             }
             else
             {
-                currentS += temp.substr(0, temp.size() - 1);
-                currentS += "ay";
-                currentS += ".";
+                t = str[i].substr(0, str[i].size() - 1) + "ay.";
+                str[i] = t;
             }
         }
         else
         {
-            currentS += temp.substr(1, temp.size());
-            currentS += temp.substr(0, 1);
-            currentS += "ay";
-            currentS += " ";
-        }
-        isVowel = false;
-        cout << currentS;
-        currentS = "";
-        if (((int)str.size() - 1) == i)
-        {
-            cout << endl;
+            string t;
+            if (str[i].at(str[i].size() - 1) != '.')
+            {
+                t = str[i].substr(1, str[i].size() - 1) + str[i].at(0);
+                t += "ay";
+                str[i] = t;
+            }
+            else
+            {
+                t = str[i].substr(1, str[i].size() - 1) + str[i].at(0);
+                t += "ay.";
+                str[i] = t;
+            }
         }
     }
+    for (int i = 0; i < sz(str); i++)
+    {
+        cout << str[i];
+        if (i != sz(str))
+        {
+            cout << " ";
+        }
+    }
+    cout << endl;
 }
