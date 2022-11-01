ad1f7667046060763815ab63faeee6545d8ee469
Bekzhan Eldiiar uulu <eldiiar_b@auca.kg>
2022-11-01 17:38:38 +0100

    lab03, p02 presentation error

--- lab03/p02/main.cpp
+++ lab03/p02/main.cpp
@@ -4,13 +4,13 @@ template <typename C>
 int sz(const C &c) { return static_cast<int>(c.size()); }
 
 using namespace std;
-void move(int x1, int y1, vector<vector<int>> &puzzle, string &command)
+void move(int x, int y, vector<vector<int>> &puzzle, string command)
 {
-    int x = x1;
-    int y = y1;
+    bool isPossible = false;
     for (int i = 0; i < sz(command); i++)
     {
-        switch (command[i])
+
+        switch (command.at(i))
         {
         case 'A':
             if (x >= 1)
@@ -20,14 +20,22 @@ void move(int x1, int y1, vector<vector<int>> &puzzle, string &command)
                 puzzle[x - 1][y] = temp;
                 x -= 1;
             }
+            else
+            {
+                isPossible = true;
+            }
             break;
         case 'B':
-            if (x < 4)
+            if (x <= 3)
             {
                 int temp = puzzle[x][y];
                 puzzle[x][y] = puzzle[x + 1][y];
                 puzzle[x + 1][y] = temp;
                 x += 1;
+            }
+            else
+            {
+                isPossible = true;
             }
             break;
         case 'L':
@@ -38,6 +46,10 @@ void move(int x1, int y1, vector<vector<int>> &puzzle, string &command)
                 puzzle[x][y - 1] = temp;
                 y -= 1;
             }
+            else
+            {
+                isPossible = true;
+            }
             break;
         case 'R':
             if (y <= 3)
@@ -47,17 +59,46 @@ void move(int x1, int y1, vector<vector<int>> &puzzle, string &command)
                 puzzle[x][y + 1] = temp;
                 y += 1;
             }
+            else
+            {
+                isPossible = true;
+            }
+            break;
+        default:
+            isPossible = true;
             break;
         }
+        if (isPossible)
+        {
+            break;
+        }
+    }
+    if (isPossible)
+    {
+        cout << "This puzzle has no final configuration." << endl;
+        cout << endl;
+    }
+    else
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                cout << (char)puzzle[i][j] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
     }
 }
 int main()
 {
     iostream::sync_with_stdio(false);
+    int numOfPuzzle = 1;
     for (string s; getline(cin, s) && s != "Z";)
     {
 
-        vector<vector<int>> puzzle(5, vector<int>(5));
+        vector<vector<int>> puzzle(5, vector<int>(5, 0));
         for (int i = 0; i < 5; i++)
         {
             puzzle[0][i] = (int)s[i];
@@ -75,36 +116,36 @@ int main()
                 break;
             }
         }
-        cin.clear();
-        string command;
-        getline(cin, command);
-        for (int i = 0; i < 5; i++)
-        {
-            for (int j = 0; j < 5; j++)
-            {
-                cout << (char)puzzle[i][j] << " ";
-            }
-            cout << endl;
-        }
+
+        int xPos = 0;
+        int yPos = 0;
+        bool isFound = false;
         for (int i = 0; i < 5; i++)
         {
             for (int j = 0; j < 5; j++)
             {
                 if (puzzle[i][j] == (int)' ')
                 {
-                    move(i, j, puzzle, command);
+                    xPos = i;
+                    yPos = j;
+                    isFound = true;
                     break;
                 }
             }
-        }
-        for (int i = 0; i < 5; i++)
-        {
-            for (int j = 0; j < 5; j++)
+            if (isFound)
             {
-                cout << (char)puzzle[i][j] << " ";
+                break;
             }
-            cout << endl;
         }
-        cout << endl;
+        string command;
+
+        for (char str; cin >> str && str != '0';)
+        {
+            command += str;
+        }
+        cin.ignore();
+        cout << "Puzzle #" << numOfPuzzle++ << ":" << endl;
+        move(xPos, yPos, puzzle, command);
+        cin.clear();
     }
 }
