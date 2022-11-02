dc66fde2540beb72329e1c0b212e25cc4395d913
Bekzhan Eldiiar uulu <eldiiar_b@auca.kg>
2022-11-02 13:21:54 +0100

    lab02, p07 fixed

--- lab02/p07/main.cpp
+++ lab02/p07/main.cpp
@@ -10,25 +10,10 @@ int main()
     iostream::sync_with_stdio(false);
     for (int a, b; cin >> a >> b && a != -1 && b != -1;)
     {
-        int firstRes = 0;
-        int secondRes = 0;
+        int first = abs(a - b);
+        int second = abs(b - a);
         if (a > b)
         {
-            firstRes = a - b;
-            secondRes = 100 - firstRes;
-        }
-        else if (a < b)
-        {
-            firstRes = b - a;
-            secondRes = 100 - firstRes;
-        }
-        if (firstRes < secondRes)
-        {
-            cout << firstRes << "\n";
-        }
-        else
-        {
-            cout << secondRes << "\n";
         }
     }
 }
