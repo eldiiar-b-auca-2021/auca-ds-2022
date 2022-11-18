30f7f578a9242bd7e0cd027d6656723ef1fe7aa1
Bekzhan Eldiiar uulu <eldiiar_b@auca.kg>
2022-11-18 04:32:23 +0100

    experiments, in exp09 the logic of multiplication is done for BigInts

--- experiments/exp09/main.cpp
+++ experiments/exp09/main.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-
+#include "../../project01/BigInt/BigInt.hpp"
 template <typename C>
 int sz(const C &c) { return static_cast<int>(c.size()); }
 
@@ -8,14 +8,17 @@ using namespace std;
 int main()
 {
     iostream::sync_with_stdio(false);
-    vector<int> v1 = {5, 9};
-    vector<int> v2 = {5, 6};
+    vector<int> v1 = {1, 2, 3};
+    vector<int> v2 = {5, 6, 5, 5};
     string res;
     int a = v1.size() - 1;
     int b = v2.size() - 1;
     int numToRem = 0;
+    int count = 0;
+    BigInt r("0");
     for (int i = b; i >= 0; i--)
     {
+        count++;
         for (int j = a; j >= 0; j--)
         {
             int t = v1[j] * v2[i] + numToRem;
@@ -27,7 +30,15 @@ int main()
             res += to_string(numToRem);
             numToRem = 0;
         }
-        cout << res << endl;
+        reverse(res.begin(), res.end());
+        r = (r + BigInt(res));
         res = "";
+        for (int k = 0; k < count; k++)
+        {
+            res += "0";
+        }
     }
+    ostringstream sout;
+    sout << r;
+    cout << sout.str() << endl;
 }
