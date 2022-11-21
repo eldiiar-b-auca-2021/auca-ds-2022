44f3390cfc9aecbfbfe7104c3710d60bedff59e5
Bekzhan Eldiiar uulu <eldiiar_b@auca.kg>
2022-11-21 14:35:56 +0100

    lab04, p01 is done

---
+++ lab04/p01/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x;
+    int y;
+    int *p;
+    x = 1;
+    p = &x;
+
+    printf("x = %d\n", x);
+    printf("address of x = %p\n", (void *)p);
+    printf("x = %d\n", *p); // dereference
+
+    *p = 2;
+    printf("x = %d\n", x);
+    printf("x = %d\n", *p);
+
+    y = *p * *p;
+    printf("y = %d\n", y);
+}
