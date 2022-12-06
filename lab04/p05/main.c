11fa9d75d24143125f7aebc468918fe742915690
Bekzhan Eldiiar uulu <eldiiar_b@auca.kg>
2022-12-06 18:35:56 +0100

    lab04, p05 is finished

--- lab04/p05/main.c
+++ lab04/p05/main.c
@@ -1,55 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 void printArray(int *beg, int *end);
 void reverseArray(int *beg, int *end);
 
 int main(void)
 {
+    // vector<int>
     int *data = NULL;
     size_t sz = 0;
     size_t cp = 0;
-    puts("enter arbitrary number of integer values: ");
-    int x;
-    while ((scanf("%d", &x) == 1))
+    int t;
+    while (scanf("%d", &t) == 1)
     {
         if (sz == cp)
         {
-            cp = (cp == 0) ? 1 : cp * 2;
+            cp = cp == 0 ? 1 : cp * 2;
             int *newData = (int *)malloc(cp * sizeof(int));
-            for (size_t i = 0; i < sz; i++)
+            for (int i = 0; i < sz; i++)
             {
-                *(newData + i) = *(data + i);
+                newData[i] = data[i];
             }
             free(data);
             data = newData;
         }
-        data[sz++] = x;
+        data[sz++] = t;
     }
     printArray(data, data + sz);
     reverseArray(data, data + sz);
     printArray(data, data + sz);
-}
-void reverseArray(int *beg, int *end)
-{
-    while (true)
-    {
-        if (beg == end)
-            break;
-        end--;
-        if (beg == end)
-            break;
-        int temp = *beg;
-        *beg = *end;
-        *end = temp;
-        beg++;
-    }
+    free(data);
 }
 void printArray(int *beg, int *end)
 {
-    for (int *b = beg; b != end; b++)
+    while (beg != end)
     {
-        printf("%d ", *b);
+        printf("%d ", *beg);
+        beg++;
     }
     printf("\n");
+}
+void reverseArray(int *beg, int *end)
+{
+    for (;;)
+    {
+        if (beg == end)
+        {
+            break;
+        }
+        end--;
+        if (beg == end)
+        {
+            break;
+        }
+        int temp = *end;
+        *end = *beg;
+        *beg = temp;
+        beg++;
+    }
 }
